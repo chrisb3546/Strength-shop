b72c875d9bbc0ae99cd4f2cc48a389e4dce63517
chrisb3546 <chrisb3546@yahoo.com>
2020-10-08 12:02:29 -0400

    first commit, building backend category, product, and orders

---
+++ db/migrate/20201008155529_create_orders.rb
@@ -0,0 +1,11 @@
+class CreateOrders < ActiveRecord::Migration[6.0]
+  def change
+    create_table :orders do |t|
+      t.string :first_name
+      t.string :last_name
+      t.float :sub_total
+
+      t.timestamps
+    end
+  end
+end
