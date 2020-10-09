81d5080e2b08f5eee6b09aa24c8bc13a9ef94b73
chrisb3546 <chrisb3546@yahoo.com>
2020-10-09 11:38:10 -0400

    add app.js template for how site will look

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,75 @@
 import React from 'react';
 import './App.css';
 
-class App extends React.Component {
-
+function App() {
   
-  
-
-  componentDidMount(){
-    fetch('http://localhost:3001/categories')
-    .then(res => res.json())
-    .then(data => console.log(data))
+  const openMenu = () => {
+    document.querySelector(".sidebar").classList.add("open")
   }
 
-  render(){
+  const closeMenu = () => {
+    document.querySelector(".sidebar").classList.remove("open")
+  }
+
+  
   return (
-    <div className="App">
-      <h1>Welcome to the strength shop</h1>
     
+ 
+    <div className="grid-container">
+      <header className="header">
+        <div className="brand">
+
+          <button onClick={openMenu}>
+            &#9776;
+          </button >
+          <a href="index.html">Strength Shop </a>
+        </div>
+        <div className="header-links">
+          <a href="login">Log in</a>
+          <a href="signup">Sign up</a>
+          <a href="orders">Your Orders</a>
+        </div>
+      </header>
+      <aside className="sidebar">
+        <h3>Equipment Categories</h3>
+        <button className="sidebar-close-button" onClick={closeMenu}>X</button>
+        <ul>
+          <li>
+            <a href="index.html">Barbells</a>
+          </li>
+
+          <li>
+            <a href="index.html">Sleds</a>
+          </li>
+
+       </ul>
+      </aside>
+      <main className="main">
+        <div className="content">
+
+        <ul className="products">
+          <li>
+            <div className="product">
+              <img className="product-image" src='/img/1500-bar.jpg' alt="product"></img>
+              <div className="product-name">
+                <a href="product.html">1500 Bar</a></div>
+              <div className="product-price">$300</div>
+              </div>
+          </li>
+         
+         
+        </ul>
+      </div>
+      </main>
     </div>
+ 
+  
+
+    
+
   );
 }
-}
 
-export default App;
+
+
+export default App
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,21 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import {createStore, applyMiddleware} from 'redux'
+import {createStore, applyMiddleware, compose} from 'redux'
 import thunk from 'redux-thunk'
 import {Provider} from 'react-redux'
+import OrderReducer from './reducers/OrderReducer'
 
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+let reducer = ''
 
+let store = createStore(OrderReducer, composeEnhancers(applyMiddleware(thunk)))
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store = {store}>
     <App />
-  </React.StrictMode>,
+    </Provider>,
   document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
